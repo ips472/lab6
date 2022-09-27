a160ddece3aac7a3e02d69eb1da6b6bbf467c6e5
ipsit <ipsitsingh61@gmial.com>
2022-09-27 09:43:02 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+interface TodoItem {
+  title: string;
+  description:string;
+  date: Date;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'my-app';
+  todoItem: string = '';
+  
+  listofTodos: TodoItem[]=[];
+  
+  addToTodoList= ()=>{
+    this.listofTodos.push({title: this.todoItem ,description:this.todoItem ,date: new Date()});
+  }
+  deleteItem= (indexofTheItem: number)=>{
+    this.listofTodos.splice(indexofTheItem,1);
+  }
 }
